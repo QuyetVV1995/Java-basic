b8e06f4aaa50163ecb6f6d60998bb6be3ca14668
QuyetVV95 <quyeta2ubqn@gmail.com>
2020-07-21 00:11:11 +0700

    hello world

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld{
+	public static void main(String[] args){
+	// In hello world tren terminal window
+	System.out.println("Hello world");
+	System.out.println("Haaaaaaa");
+	}
+}
