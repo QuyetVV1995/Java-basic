cdea72cdf9fca9720533af79be2b4cd5604150bd
QuyetVV95 <quyeta2ubqn@gmail.com>
2020-07-21 16:51:30 +0700

    variable

---
+++ variable.java
@@ -0,0 +1,16 @@
+public class variable{
+	public static void main(String[] args) {
+		
+		String name;
+		float height;
+		int age;
+
+		name = "Quyet";
+		height = 165f;
+		age = 25;
+
+		System.out.println(name);
+		System.out.println(height);
+		System.out.println(age);
+	}
+}
