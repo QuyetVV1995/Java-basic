21a4d6c78c8527919999557220e87fdada98103b
QuyetVV95 <quyeta2ubqn@gmail.com>
2020-07-21 23:27:03 +0700

    if else trong Java

--- Readme.txt
+++ Readme.txt
@@ -2,3 +2,9 @@
 final <kieu du lieu><ten bien> = <gia tri hang>;
 *ep kieu theo cu phap
 (<kieu du lieu>) <ten bien>;
+
+* cu phap re nhanh
+if(<bieu thuc dieu kien>) 
+<cau lech thuc hien dieu kien dung>
+else
+<cau lenh thuc hien dieu kien sai>
---
+++ ifelsetrongJava.java
@@ -0,0 +1,13 @@
+public class ifelsetrongJava{
+	public static void main(String[] args) {
+		String job = "Sinh vien";
+		if (job == "Hoc sinh") {
+			System.out.println("Ban la hoc sinh");
+		}
+		else if (job == "Sinh vien") {
+			System.out.println("Ban la sinh vien");
+		}
+		else
+			System.out.println("Ban la ai");
+	}
+}
